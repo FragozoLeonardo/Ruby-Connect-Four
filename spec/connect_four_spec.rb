e6642ea061943c91fe6025081ec724676b10cbc0
Leonardo Fragozo <leo.fragozo.dev@gmail.com>
2023-08-14 13:46:09 -0300

    Oops wrong file lol

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -1,6 +1,6 @@
 # spec/connect_four_spec.rb
 
-require_relative '../connect_four'
+require_relative './connect_four'
 
 RSpec.describe ConnectFour do
   describe "#new" do
@@ -10,6 +10,67 @@ RSpec.describe ConnectFour do
     end
   end
 
-  # Add more example blocks here as you develop your game
+  describe "#make_move" do
+    context "when a valid move is made" do
+      it "places the player's piece in the specified column" do
+        game = ConnectFour.new
+        game.make_move(1)
+        expect(game.board[5][0]).to eq("\u25CF")
+      end
+    end
+
+    context "when an invalid move is made" do
+      it "returns false and does not change the board" do
+        game = ConnectFour.new
+        initial_board = game.board.dup
+        result = game.make_move(8)
+        expect(result).to be_falsey
+        expect(game.board).to eq(initial_board)
+      end
+    end
+  end
+
+  describe "#check_win" do
+    context "when there is a vertical win" do
+      it "returns true" do
+        game = ConnectFour.new
+        4.times { game.make_move(1) }
+        expect(game.check_win).to be_truthy
+      end
+    end
+
+    context "when there is a horizontal win" do
+      it "returns true" do
+        game = ConnectFour.new
+        (1..4).each { |col| game.make_move(col) }
+        expect(game.check_win).to be_truthy
+      end
+    end
+
+    context "when there is a diagonal win (positive slope)" do
+      it "returns true" do
+        game = ConnectFour.new
+        (1..3).each { |col| 3.times { game.make_move(col) } }
+        game.make_move(4)
+        expect(game.check_win).to be_truthy
+      end
+    end
+
+    context "when there is a diagonal win (negative slope)" do
+      it "returns true" do
+        game = ConnectFour.new
+        game.make_move(4)
+        (1..3).each { |col| 3.times { game.make_move(col) } }
+        expect(game.check_win).to be_truthy
+      end
+    end
+
+    context "when there is no win" do
+      it "returns false" do
+        game = ConnectFour.new
+        3.times { game.make_move(1) }
+        expect(game.check_win).to be_falsey
+      end
+    end
+  end
 end
-cdc
